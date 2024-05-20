297de93f0b6996f591671cae357d0f854e05511c
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-20 09:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const obj = {}
+    for (let ch of str) {
+        let currChar = obj[ch];
+        //if charecter is available return +1 or else return 1
+        
+        obj[ch] = currChar?obj[ch]+1:1;
+    }
+    const arr = [];
+    let isFind = false;
+    
+    for (let key in obj) {
+      if(obj[key] == 1){
+        arr.push(key);
+        isFind = true;
+      }
+    }
+    if(isFind){
+      return arr;
+    }else{
+      return null;
+    }
+    
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
